36d2e46f7aaa0552bbe3bc593f6492c59b5fafb5
HaruyamaRiku <shybatter@gmail.com>
2026-02-22 14:17:55 +0900

    環境構築

---
+++ household-account-book/src/App.tsx
@@ -0,0 +1,13 @@
+const App = () => {
+  return (
+    <>
+      <h1>Hello, React!</h1>
+      <p>これはReactで作成したアプリです。</p>
+    </>
+  );
+};
+
+export default App;
+
+// git config --global user.name HaruyamaRiku
+// git config --global user.email shybatter@gmail.com
